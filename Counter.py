f256541f6e30fd9b259ce97c81337caab57fc767
DatSimonGuy <62205459+Lazerpl12@users.noreply.github.com>
2023-05-15 00:34:44 +0200

    full working code

--- Counter.py
+++ Counter.py
@@ -14,6 +14,34 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Nya~")
 
-chat = -1001153386748
+names = []
+messages = []
+
+
+def Display():
+    i = 0
+    for name in names:
+        print(name + f' {messages[i]}')
+        i+=1
+
+chat = int(os.environ.get('CHAT'))
+api_id = os.environ.get('ID')
+api_hash = os.environ.get('HASH')
+
+
+
+from telethon.sync import TelegramClient
+
+client = TelegramClient('session_id', api_id, api_hash)
+n = 0
+with client:
+    for msg in client.iter_messages(chat, 100000):
+        n+=1
+        print(n)
+        if n%10000 == 0:
+            Display()
+        if msg.sender.first_name in names:
+            messages[names.index(msg.sender.first_name)] += 1
+
 
 bot.infinity_polling()
